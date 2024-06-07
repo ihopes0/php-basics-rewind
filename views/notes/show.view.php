1e98293ef8f97c6521ad870072bca5189ff0fd24
ihopeso <brnv.ma@gmail.com>
2024-06-07 22:09:33 +1000

    implement beta note deletion feature

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -7,6 +7,10 @@
 <main>
   <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
     <p><?= htmlspecialchars($note['body']) ?></p>
+    <form method="POST" class="mt-5">
+      <input type="hidden" name="id" value="<?= $note['id']?>">
+      <button class="text-sm text-red-600">Delete note</button>
+    </form>
     <br>
     <a href="/notes" class='underline' style="background-color: #909090; padding:5px; border:2px solid black; border-radius: 7px">go back</a>
   </div>
