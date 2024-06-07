c5201755b381cac9faf8d451396f504aaed8d7d2
ihopeso <brnv.ma@gmail.com>
2024-06-07 20:08:29 +1000

    refactor validatiot, create separate Validator class

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,4 +1,5 @@
 <?php
+require 'Validator.php';
 
 $config = require 'config.php';
 $db = new Database($config['database']);
@@ -6,16 +7,13 @@ $db = new Database($config['database']);
 
 $heading = "Create Note";
 
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $errors = [];
 
-    if (strlen($_POST['body']) == 0) {
-        $errors['body'] = "A body is required";
-    }
-
-    if (strlen($_POST['body']) > 500) {
-        $errors['body'] = "The body cannot be more than 500 characters.";
+    if (! Validator::string($_POST['body'], 1, 1000)) {
+        $errors['body'] = "A body of no more than 1000 characters is required";
     }
 
     if (empty($errors)) {
