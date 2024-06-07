4d8dc51b502997b903b71381c0e9a7d0681f0d5d
ihopeso <brnv.ma@gmail.com>
2024-06-07 20:21:32 +1000

    move note controllers and views to notes directory, adjust routes accordingly

--- views/notes/create.view.php
+++ views/notes/create.view.php
@@ -1,8 +1,8 @@
-<?php require("partials/header.php"); ?>
+<?php require("views/partials/header.php"); ?>
 
-<?php require("partials/nav.php"); ?>
+<?php require("views/partials/nav.php"); ?>
 
-<?php require("partials/banner.php"); ?>
+<?php require("views/partials/banner.php"); ?>
 
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
@@ -41,4 +41,4 @@
     </div>
 </main>
 
-<?php require("partials/footer.php"); ?>
+<?php require("views/partials/footer.php"); ?>
