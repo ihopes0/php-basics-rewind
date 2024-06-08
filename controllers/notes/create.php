5c40529c630705bb18ecfa6e3db8ed617da338b4
ihopeso <brnv.ma@gmail.com>
2024-06-08 13:54:20 +1000

    divide create get and post controllers

--- controllers/notes/create.php
+++ controllers/notes/create.php
@@ -1,30 +1,6 @@
 <?php
 
-use Core\Database;
-use Core\Validator;
-
-$config = require base_path('config.php');
-$db = new Database($config['database']);
-
-$errors = [];
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    if (! Validator::string($_POST['body'], 1, 1000)) {
-        $errors['body'] = "A body of no more than 1000 characters is required";
-    }
-
-    if (empty($errors)) {
-        $db->query("INSERT INTO notes(body, user_id) VALUES(:body, :user_id);", [
-            'body' => $_POST['body'],
-            'user_id' => 1
-        ]);
-    }
-    header('location: /notes');
-    exit();
-}
-
 view("views/notes/create.view.php", [
     'heading' => 'Create Note',
-    'errors' => $errors
+    'errors' => []
 ]);
