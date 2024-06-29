0ad9e23eceac46f801f561461a94cc4c4816e5ce
ihopeso <brnv.ma@gmail.com>
2024-06-29 18:12:22 +1000

    extract searching for old info into separate utility function

--- views/session/create.view.php
+++ views/session/create.view.php
@@ -23,7 +23,7 @@ use Core\Session;
                                autocomplete="email" 
                                required 
                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                               value="<?= $_SESSION['_flash']['old']['email'] ?? '' ?>"
+                               value="<?= old('email') ?>"
                                >
                     </div>
                     <?php if (isset($errors['email'])) : ?>
@@ -42,7 +42,7 @@ use Core\Session;
                                autocomplete="current-password" 
                                required 
                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                               value="<?= $_SESSION['_flash']['old']['password'] ?? '' ?>"
+                               value="<?= old('password')?>"
                                >
                     </div>
                     <?php if (isset($errors['password'])) : ?>
